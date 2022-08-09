aa62b7f8acfc6a2c0d845014a389fa210acd2ef0
Matt Sheffield <matt.sheffield@campuslogic.awesome>
2022-08-09 10:10:03 -0700

    fixed images not loading and cleaned up html

--- memory.js
+++ memory.js
@@ -1,14 +1,14 @@
-function arrayRemove(arr, value) { 
-     return arr.filter(function(ele){ 
-         return ele != value; 
-     });
- }
+function arrayRemove(arr, value) {
+    return arr.filter(function (ele) {
+        return ele != value;
+    });
+}
 
 
 
 let container = document.querySelector('.game-container');
 let html = `<div class="flip-card"> <div class="flip-card-inner"> <div class="flip-card-front" style="background-image: url(https://m.media-amazon.com/images/M/MV5BMzMwYjc1N2MtY2U2Ny00MTc3LTk1YWQtYzE3NmM5NWQ2YzkyXkEyXkFqcGdeQXVyMzAzODY0NzE@._V1_.jpg); background-repeat: no-repeat; background-size: cover;"></div><div class="flip-card-back" style="background-repeat: no-repeat; background-size: cover;"> <h1></h1> </div></div></div>`;
-let imageArray = ["ironManLogo","batManLogo","captainAmericaLogo","deadPoolLogo","flashLogo","greenLanternLogo","scottLogo","spiderManLogo","superManLogo","wonderWomanLogo"];
+let imageArray = ["ironManLogo", "batManLogo", "captainAmericaLogo", "deadPoolLogo", "flashLogo", "greenLanternLogo", "scottLogo", "spiderManLogo", "superManLogo", "wonderWomanLogo"];
 for (let index = 0; index < 8; index++) {
     let card = document.createElement("div");
     let idx = Math.floor(Math.random() * imageArray.length);
@@ -16,7 +16,7 @@ for (let index = 0; index < 8; index++) {
     card.innerHTML = html;
     let background = card.querySelector('.flip-card-back');
     container.append(card);
-    background.style.backgroundImage = "url('/images/" + imageArray[idx] + ".png')";
-    arrayRemove(imageArray,imageArray[idx]);
+    background.style.backgroundImage = "url('images/" + imageArray[idx] + ".png')";
+    arrayRemove(imageArray, imageArray[idx]);
 }
 
