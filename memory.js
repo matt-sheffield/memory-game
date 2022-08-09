db86551910db478ff31fcff3185c5cf0e0975e4f
Chiren Shah <shah.chiren25@gmail.com>
2022-08-09 16:01:23 -0700

    made the random function working

--- memory.js
+++ memory.js
@@ -2,13 +2,8 @@
 let cardFlipped = false;
 let lockBoard = false;
 let firstCard, secondCard;
-
-// Function that removes an element from an array once it has been selected.
-function arrayRemove(arr, value) { 
-    return arr.filter(function(ele){ 
-        return ele != value; 
-    });
-}
+let val  = 12 // entered by user has to be even
+let card_pop = Math.min(20,val);
 
 //Initializing game container to container 
 let container = document.querySelector('.game-container');
@@ -16,7 +11,8 @@ let container = document.querySelector('.game-container');
 let html = `<div class="flip-card"> <div class="flip-card-inner"> <div class="flip-card-front" style="background-image: url(https://m.media-amazon.com/images/M/MV5BMzMwYjc1N2MtY2U2Ny00MTc3LTk1YWQtYzE3NmM5NWQ2YzkyXkEyXkFqcGdeQXVyMzAzODY0NzE@._V1_.jpg); background-repeat: no-repeat; background-size: cover;"></div><div class="flip-card-back" style="background-repeat: no-repeat; background-size: cover;"> <h1></h1> </div></div></div>`;
 //Populating image array with images folder names
 let imageArray = ["ironManLogo","batManLogo","captainAmericaLogo","deadPoolLogo","flashLogo","greenLanternLogo","scottLogo","spiderManLogo","superManLogo","wonderWomanLogo"];
-
+imageArray = imageArray.flatMap(i => [i,i]);
+imageArray = imageArray.splice(0,card_pop)
 /*
  For loop that populates web page with cards using imageArray and randomly generates
  a number to select the index of the array for a photo. 
@@ -26,12 +22,12 @@ function flipCard(){
     this.classList.toggle("flipCard");
     if(true){ // function to check if it's a match
         setTimeout(()=>{
-            this.classList.toggle("flipCard");
+            //this.classList.toggle("flipCard");
         },1000)
     }
 }
 
-for (let index = 0; index < 12; index++) {
+for (let index = 0; index < card_pop; index++) {
    let card = document.createElement("div");
    let idx = Math.floor(Math.random() * imageArray.length);
    card.className = "col-xs-6 col-sm-6 col-md-3";
@@ -41,11 +37,5 @@ for (let index = 0; index < 12; index++) {
    let flip = card.querySelector('.flip-card-inner');
    flip.addEventListener("click", flipCard);
    background.style.backgroundImage = "url('/images/" + imageArray[idx] + ".png')";
-   arrayRemove(imageArray,imageArray[idx]);
+   imageArray.splice(idx,1);
 }
-
-//Initialize first card to flip on click
-// cards = 
-// cards.addEventListener("click", flipCard);
-
-//Function for toggling flip card class from memory.css
