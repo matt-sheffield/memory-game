960a9fad1eccdc7bf8a395b4063370c1b46e992b
matt-sheffield <matthew.sheffield75@gmail.com>
2022-08-08 20:57:41 -0700

    dynamic images

--- memory.js
+++ memory.js
@@ -1,21 +1,22 @@
-// function arrayRemove(arr, value) { 
-//     return arr.filter(function(ele){ 
-//         return ele != value; 
-//     });
-// }
+function arrayRemove(arr, value) { 
+     return arr.filter(function(ele){ 
+         return ele != value; 
+     });
+ }
 
 
 
 let container = document.querySelector('.game-container');
-let html = `<div class="flip-card"> <div class="flip-card-inner"> <div class="flip-card-front" style="background-image: url(https://m.media-amazon.com/images/M/MV5BMzMwYjc1N2MtY2U2Ny00MTc3LTk1YWQtYzE3NmM5NWQ2YzkyXkEyXkFqcGdeQXVyMzAzODY0NzE@._V1_.jpg); background-repeat: no-repeat; background-size: cover;"></div><div class="flip-card-back" style="background-image: url(https://storage.googleapis.com/oceanwide_web/media-dynamic/cache/widen_1100_progressive/media/default/0001/05/9eb2fb74dcf9dd4e20af036f805ead7707f969c6.jpeg); background-repeat: no-repeat; background-size: cover;"> <h1></h1> </div></div></div>`;
-let imageArray = ["aquaManLogo","batManLogo","captainAmericaLogo"]
-for (let index = 0; index < 20; index++) {
+let html = `<div class="flip-card"> <div class="flip-card-inner"> <div class="flip-card-front" style="background-image: url(https://m.media-amazon.com/images/M/MV5BMzMwYjc1N2MtY2U2Ny00MTc3LTk1YWQtYzE3NmM5NWQ2YzkyXkEyXkFqcGdeQXVyMzAzODY0NzE@._V1_.jpg); background-repeat: no-repeat; background-size: cover;"></div><div class="flip-card-back" style="background-repeat: no-repeat; background-size: cover;"> <h1></h1> </div></div></div>`;
+let imageArray = ["ironManLogo","batManLogo","captainAmericaLogo","deadPoolLogo","flashLogo","greenLanternLogo","scottLogo","spiderManLogo","superManLogo","wonderWomanLogo"];
+for (let index = 0; index < 8; index++) {
     let card = document.createElement("div");
     let idx = Math.floor(Math.random() * imageArray.length);
-    let background = document.querySelector('.flip-card-back');
     card.className = "col-xs-6 col-sm-6 col-md-3";
     card.innerHTML = html;
-    // background.style.backgroundImage = "url('/images/" + imageArray[idx] + ".png')";
+    let background = card.querySelector('.flip-card-back');
     container.append(card);
+    background.style.backgroundImage = "url('/images/" + imageArray[idx] + ".png')";
+    arrayRemove(imageArray,imageArray[idx]);
 }
 
