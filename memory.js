292d1196ddba1cb3b4a624a6def5fcc5bf0ea4eb
romeobarrientos <rgbarrie@asu.edu>
2022-08-09 13:02:21 -0700

    Added comments and created flip function
    Cleaned up the code a bit to make it more readable and added comments to make the logic understandable.

--- memory.js
+++ memory.js
@@ -1,15 +1,27 @@
+//Initializing variables for future functions
+let cardFlipped = false;
+let lockBoard = false;
+let firstCard, secondCard;
+
+// Function that removes an element from an array once it has been selected.
 function arrayRemove(arr, value) { 
     return arr.filter(function(ele){ 
         return ele != value; 
     });
 }
 
-
-
+//Initializing game container to container 
 let container = document.querySelector('.game-container');
+//Establishing design of card using html file
 let html = `<div class="flip-card"> <div class="flip-card-inner"> <div class="flip-card-front" style="background-image: url(https://m.media-amazon.com/images/M/MV5BMzMwYjc1N2MtY2U2Ny00MTc3LTk1YWQtYzE3NmM5NWQ2YzkyXkEyXkFqcGdeQXVyMzAzODY0NzE@._V1_.jpg); background-repeat: no-repeat; background-size: cover;"></div><div class="flip-card-back" style="background-repeat: no-repeat; background-size: cover;"> <h1></h1> </div></div></div>`;
+//Populating image array with images folder names
 let imageArray = ["ironManLogo","batManLogo","captainAmericaLogo","deadPoolLogo","flashLogo","greenLanternLogo","scottLogo","spiderManLogo","superManLogo","wonderWomanLogo"];
-for (let index = 0; index < 8; index++) {
+
+/*
+ For loop that populates web page with cards using imageArray and randomly generates
+ a number to select the index of the array for a photo. 
+*/
+for (let index = 0; index < 12; index++) {
    let card = document.createElement("div");
    let idx = Math.floor(Math.random() * imageArray.length);
    card.className = "col-xs-6 col-sm-6 col-md-3";
@@ -20,3 +32,11 @@ for (let index = 0; index < 8; index++) {
    arrayRemove(imageArray,imageArray[idx]);
 }
 
+//Initialize first card to flip on click
+firstCard = document.querySelector('.flip-card-inner');
+firstCard.addEventListener("click", flipCard);
+
+//Function for toggling flip card class from memory.css
+function flipCard(){
+    firstCard.classList.toggle("flipCard");
+}
